52029d3325449d839125a0e15584fbbd7d3b7df1
Eduardo Garcia <eduardo.garcia@liferay.com>
2017-11-16 18:11:16 +0100

    LPS-75962 Fix reference

--- analytics-client-osgi/src/main/java/com/liferay/analytics/client/osgi/internal/AnalyticsClientImpl.java
+++ analytics-client-osgi/src/main/java/com/liferay/analytics/client/osgi/internal/AnalyticsClientImpl.java
@@ -21,6 +21,12 @@ import com.liferay.petra.json.web.service.client.JSONWebServiceClient;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.service.component.ComponentFactory;
+import org.osgi.service.component.ComponentInstance;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -29,6 +35,28 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(immediate = true, service = AnalyticsClient.class)
 public class AnalyticsClientImpl implements AnalyticsClient {
+
+	@Activate
+	public void activate() {
+		final Properties properties = new Properties();
+
+		properties.setProperty("hostName", _ANALYTICS_GATEWAY_HOST);
+		properties.setProperty("hostPort", _ANALYTICS_GATEWAY_PORT);
+		properties.setProperty("protocol", _ANALYTICS_GATEWAY_PORT);
+
+		ComponentInstance componentInstance = _componentFactory.newInstance(
+			(Dictionary)properties);
+
+		componentInstance.getInstance();
+
+		_jsonWebServiceClient =
+			(JSONWebServiceClient)componentInstance.getInstance();
+
+		_jsonWebServiceClient.setProtocol(_ANALYTICS_GATEWAY_PROTOCOL);
+		_jsonWebServiceClient.setHostName(_ANALYTICS_GATEWAY_HOST);
+		_jsonWebServiceClient.setHostPort(
+			Integer.valueOf(_ANALYTICS_GATEWAY_PORT));
+	}
 
 	public String sendAnalytics(AnalyticsEventsMessage analyticsEventsMessage)
 		throws Exception {
@@ -45,13 +73,22 @@ public class AnalyticsClientImpl implements AnalyticsClient {
 					_ANALYTICS_GATEWAY_PATH));
 		}
 
-		_jsonWebServiceClient.setProtocol(_ANALYTICS_GATEWAY_PROTOCOL);
-		_jsonWebServiceClient.setHostName(_ANALYTICS_GATEWAY_HOST);
-		_jsonWebServiceClient.setHostPort(
-			Integer.valueOf(_ANALYTICS_GATEWAY_PORT));
-
 		return _jsonWebServiceClient.doPostAsJSON(
 			_ANALYTICS_GATEWAY_PATH, jsonAnalyticsEventsMessage);
+	}
+
+	@Reference(
+		target = "(component.factory=JSONWebServiceClient)", unbind = "-"
+	)
+	protected void setComponentFactory(ComponentFactory componentFactory) {
+		_componentFactory = componentFactory;
+	}
+
+	@Reference(unbind = "-")
+	protected void setJSONObjectMapper(
+		JSONObjectMapper<AnalyticsEventsMessage> jsonObjectMapper) {
+
+		_jsonObjectMapper = jsonObjectMapper;
 	}
 
 	private static final String _ANALYTICS_GATEWAY_HOST = System.getProperty(
@@ -70,10 +107,8 @@ public class AnalyticsClientImpl implements AnalyticsClient {
 	private static final Log _log = LogFactoryUtil.getLog(
 		AnalyticsClientImpl.class);
 
-	@Reference
+	private ComponentFactory _componentFactory;
 	private JSONObjectMapper<AnalyticsEventsMessage> _jsonObjectMapper;
-
-	@Reference
 	private JSONWebServiceClient _jsonWebServiceClient;
 
 }
